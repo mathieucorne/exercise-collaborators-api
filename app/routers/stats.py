f03ca2fb4a0d6fc9945a90eb0febb67da9876219
Mathieu Corne <math.corne@gmail.com>
2025-11-10 05:07:53 +0100

    [ROUTES] fix: Stats route using only Adult Users informations

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -62,7 +62,7 @@ async def get_stats(team: str = None):
             }
         }
     """
-    users : list[User] = user_service.get_users()
+    users : list[User] = user_service.get_adult_users()
     total_users = len(users)
     if len(users) == 0:
         logger_service.warning("HTTP Request - get_stats: No Users Data Available")
